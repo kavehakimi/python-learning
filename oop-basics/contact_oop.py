24c0d65b8c22ed35a990357b4428a6138f4f5996
Kaveh Hakimi Firooz <khakimi@gmail.com>
2026-06-07 12:46:23 +0330

    Complete CRUD operations in OOP contact manager

--- oop-basics/contact_oop.py
+++ oop-basics/contact_oop.py
@@ -32,7 +32,7 @@ def add_contact():
     mobile = input("Enter mobile: ")
     email = input("Enter email: ")
 
-    new_contact = contact(name, mobile, email)
+    new_contact = Contact(name, mobile, email)
     contacts.append(new_contact)
     save_contacts_to_file()
     print("Contact added successfully!")
@@ -41,7 +41,7 @@ def view_contacts():
     if len(contacts) == 0:
         print("There is no contact!")
         return
-      
+    
     for contact in contacts:
         contact.display()
 
@@ -61,17 +61,60 @@ def load_contacts_from_file():
             datalist = json.load(f)
 
         for data in datalist:
-            contacts.append(contact.from_dict(data))
-
+            contacts.append(Contact.from_dict(data))
+    
     except FileNotFoundError:
         contacts = []
+
+def search_contact():
+    name_to_search = input("\nEnter name to search: ")
+
+    found = False
+
+    for contact in contacts:
+        if contact.name.lower() == name_to_search.lower():
+            contact.display()
+            found = True
+
+    if not found:
+        print("Contact not found!")
+
+def delete_contact():
+    name_to_delete = input("\nEnter name to delete: ")
+
+    for contact in contacts:
+        if contact.name.lower() == name_to_delete.lower():
+            contacts.remove(contact)
+            save_contacts_to_file()
+            print("Contact deleted successfully!")
+            return
+    
+    print("Contact not found!")
+
+def update_contact():
+    name_to_update = input("\nEnter name to update: ")
+    for contact in contacts:
+        if contact.name.lower() == name_to_update.lower():
+            contact.name = input("Enter new name: ")
+            contact.mobile = input("Enter new mobile: ")
+            contact.email = input("Enter new email: ")
+
+            save_contacts_to_file()
+            print("Contact updated successfully!")
+            return
+
+    print("Contact not found!")
+
 
 load_contacts_from_file()
 
 while True:
     print("\n1. Add Contact")
     print("2. View Contacts")
-    print("3. Exit")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Update Contact")
+    print("6. Exit")
 
     option = input("Choose your option: ")
 
@@ -80,6 +123,12 @@ while True:
     elif option == "2":
         view_contacts()
     elif option == "3":
+        search_contact()
+    elif option == "4":
+        delete_contact()
+    elif option == "5":
+        update_contact()
+    elif option == "6":
         break
     else:
         print("Invalid Option!")
